5acc031d2d3152717ab6951ef5ec4322f7aacc4b
CK Chuang <dissaivent@gmail.com>
2025-01-02 15:58:45 +0800

    chore: modify vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,16 @@
 /// <reference types="vitest/config" />
 import path from 'path';
-import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
   test: {
-    environment: 'happy-dom', // or "jsdom" for component testing
     globals: true,
-    setupFiles: ['./vitest.setup.ts'],
     include: ['src/**/*.test.{ts,tsx}'],
-    coverage: {
-      enabled: true,
-      reporter: ['text', 'json', 'html'],
-    },
     silent: true, // Suppresses all output except for errors
     alias: {
       '@/': new URL('./src/', import.meta.url).pathname,
