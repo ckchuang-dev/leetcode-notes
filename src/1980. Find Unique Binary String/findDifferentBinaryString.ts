234b9914103f7e53cac5a232e6c746012e7f0318
CK Chuang <dissaivent@gmail.com>
2025-02-20 15:03:32 +0800

    feat: add comments for 1980

--- src/1980. Find Unique Binary String/findDifferentBinaryString.ts
+++ src/1980. Find Unique Binary String/findDifferentBinaryString.ts
@@ -1,42 +1,45 @@
+// Method 1. Recursively generate binary string
+// time complexity: O((2^n) * n)
+// space complexity: O(n)
+function findDifferentBinaryStringRecursive(nums: string[]): string {
+    // edge case handling
+    if (nums.length === 0) {
+        return '';
+    }
+
+    // get n
+    const numLength = nums[0].length;
+
+    let matchedStr = '';
+    let found = false;
+
+    // find any match string and early return
+    const findMatchStr = (len: number, currentStr: string) => {
+        if (found) {
+            return;
+        }
+        if (currentStr.length === numLength && !nums.includes(currentStr)) {
+            matchedStr = currentStr;
+            found = true;
+            return;
+        }
+
+        if (currentStr.length < numLength) {
+            findMatchStr(len + 1, currentStr + '0');
+            findMatchStr(len + 1, currentStr + '1');
+        }
+    }
+
+    // recursively gen pair
+    findMatchStr(0, '');
+
+    return matchedStr;
+};
+
+// Method 2. Convert to number Set
+// time complexity: O(n)
+// space complexity: O(n)
 function findDifferentBinaryString(nums: string[]): string {
-  // edge case handling
-  if (nums.length === 0) {
-    return '';
-  }
-
-  // get n
-  const numLength = nums[0].length;
-
-  let matchedStr = '';
-
-  // early return flag
-  let found = false;
-
-  // find any match string and early return
-  const findMatchStr = (len: number, currentStr: string) => {
-    // early return when the first solution found
-    if (found) {
-      return;
-    }
-    if (currentStr.length === numLength && !nums.includes(currentStr)) {
-      matchedStr = currentStr;
-      found = true;
-      return;
-    }
-
-    if (currentStr.length < numLength) {
-      findMatchStr(len + 1, currentStr + '0');
-      findMatchStr(len + 1, currentStr + '1');
-    }
-  };
-
-  // recursively gen pair
-  findMatchStr(0, '');
-
-  return matchedStr;
-}
-
-function findDifferentBinaryStringNumber(nums: string[]): string {
   const integers = new Set<number>();
 
   // 轉換 nums 裡的二進位字串為數字，存入 Set
@@ -60,13 +63,15 @@ function findDifferentBinaryStringNumber(nums: string[]): string {
   return '';
 }
 
-// Cantor's Diagonal Argument
-function findDifferentBinaryStringWow(nums: string[]): string {
+// Method 3. Cantor's Diagonal Argument
+// time complexity: O(n)
+// space complexity: O(1)
+function findDifferentBinaryStringCantor(nums: string[]): string {
   const len = nums.length;
-  const ans = Array.from({ length: len }, () => '');
+  const ans = Array.from({length: len}, () => '');
 
   for (let i = 0; i < nums.length; i++) {
-    ans[i] = nums[i][i] === '0' ? '1' : '0';
+      ans[i] = nums[i][i] === '0' ? '1' : '0';
   }
 
   return ans.join('');
